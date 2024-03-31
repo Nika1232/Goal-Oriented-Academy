f2ec0a3fdd24ebac038460a64d0502d5269db4ea
ddfgdf6 <165582473+ddfgdf6@users.noreply.github.com>
2024-03-31 21:57:09 +0400

    Add files via upload

---
+++ gela.py
@@ -0,0 +1,13 @@
+name = "გელა"
+surname = "გელაძე"
+age = 22
+height = 178.8
+knows_nothing = True
+is_guly = False
+
+print ("მე მქვია" + " " + name + " " + surname + " " + "ვარ" + " " + str(age) + " " "წლის" + " " +
+       "ჩემი სიმაღლეა" + " " + str(height))
+print (type(surname))
+print (type(age))
+print (type(height))
+print (type(is_guly))
