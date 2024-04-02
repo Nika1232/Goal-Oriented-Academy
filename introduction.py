fa90a0475368cc88d056d43b2d030a5a80265310
ddfgdf6 <165582473+ddfgdf6@users.noreply.github.com>
2024-04-02 12:29:07 +0400

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,8 @@
+name = "ნიკა"
+surname = "თავართქილაძე"
+age = 22
+height = 178.8
+knows_nothing = True
+is_guly = False
+
+print (name + " " + surname + ", " + " " + "წელი" + " " + str(age) + ", " + "სიმაღლე" + " " + str(height))
