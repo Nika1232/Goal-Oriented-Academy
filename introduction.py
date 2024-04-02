5d53023b7660aa87e25902bbbf8a1240f9f28032
ddfgdf6 <165582473+ddfgdf6@users.noreply.github.com>
2024-04-02 13:12:10 +0400

    Delete introduction.py

--- introduction.py
+++
@@ -1,8 +0,0 @@
-name = "ნიკა"
-surname = "თავართქილაძე"
-age = 22
-height = 178.8
-knows_nothing = True
-is_guly = False
-
-print (name + " " + surname + ", " + " " + "წელი" + " " + str(age) + ", " + "სიმაღლე" + " " + str(height))
