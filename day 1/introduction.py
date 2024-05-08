8e7eadb7afc676a618c7b7cbd8a4b8b0e263fe8b
nika tavartqiladze <165582473+Nika1232@users.noreply.github.com>
2024-05-08 18:07:59 +0400

    Update introduction.py

--- day 1/introduction.py
+++ day 1/introduction.py
@@ -1,6 +1,6 @@
 name = "ნიკა"
 surname = "თავართქილაძე"
-age = 22
+age = 25
 height = 178.8
 
 print (name + " " + surname + ", " + " " + "წელი" + " " + str(age) + ", " + "სიმაღლე" + " " + str(height))
