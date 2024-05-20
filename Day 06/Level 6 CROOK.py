d1c170f4b9dabe0ac3dc3b9128bb80806465a31c
nika tavartqiladze <165582473+Nika1232@users.noreply.github.com>
2024-05-20 19:23:29 +0400

    Add files via upload

---
+++ Day 06/Level 6 CROOK.py
@@ -0,0 +1,121 @@
+#1) მომხმარებელს შემოატანინეთ მთელი რიცხვი და შეინახეთ ცვლადში. 
+#აიღეთ ახალი ცვლადი და პირველი ცვლადის მნიშვნელობა შეინახეთ ათწილადის სახით. საბოლოოდ შეადარეთ ეს ორი რიცხვი == ოპერატორით და გამოიტანეთ ტერმინალში.
+user = int(input("chawere mteli ricxvi: "))
+print("chawerili ricxvi gadaiqca floatad,")
+user2 = float(user)
+print(user==user2)
+print("shesrulda moqmedeba chawerili ricxvi udris tu ara gadayvanil float numbers da terminalshi gamovida shesabamisi pasuxi")
+#2) ჩამოწერეთ ხუთი მაგალითი > ოპერატორზე.
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(num1, num2):
+    product = num1 > num2
+    if num1>num2:
+        return product
+result = ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(4, 3)
+result2 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(6, 5)
+result3 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(8, 7)
+result4 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(10, 9)
+result5 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობა_ოპერატორზე(12, 11)
+print("the result is: ", result)
+print("the result is: ", result2)
+print("the result is: ", result3)
+print("the result is: ", result4)
+print("the result is: ", result5)
+#3) ჩამოწერეთ ხუთი მაგალითი >= ოპერატორზე.
+print("davaleba 3)")
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(num1, num2):
+    product = num1 >= num2
+    if num1>=num2:
+        return product
+result6 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 3)
+result7 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(6, 6)
+result8 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(8, 7)
+result9 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(10, 10)
+result10 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(12, 11)
+print("the result is: ", result6)
+print("the result is: ", result7)
+print("the result is: ", result8)
+print("the result is: ", result9)
+print("the result is: ", result10)
+#4) ჩამოწერეთ ხუთი მაგალითი < ოპერატორზე.
+print("davaleba 4)")
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(num1, num2):
+    product = num1 < num2
+    if num1<num2:
+        return product
+result6 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 5)
+result7 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(6, 51)
+result8 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(8, 9)
+result9 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(10, 11)
+result10 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(12, 13)
+print("the result is: ", result6)
+print("the result is: ", result7)
+print("the result is: ", result8)
+print("the result is: ", result9)
+print("the result is: ", result10)
+#5) ჩამოწერეთ ხუთი მაგალითი <= ოპერატორზე.
+print("davaleba 5)")
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(num1, num2):
+    product = num1 <= num2
+    if num1<=num2:
+        return product
+result6 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(5, 5)
+result7 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(6, 7)
+result8 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(8, 12)
+result9 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(10, 10)
+result10 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(12, 12)
+print("the result is: ", result6)
+print("the result is: ", result7)
+print("the result is: ", result8)
+print("the result is: ", result9)
+print("the result is: ", result10)
+#6) ჩამოწერეთ ხუთი მაგალითი == ოპერატორზე.
+print("davaleba 6")
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(num1, num2):
+    product = num1 == num2
+    if num1==num2:
+        return product
+result6 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(5, 5)
+result7 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(6, 6)
+result8 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(8, 8)
+result9 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(10, 10)
+result10 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(12, 12)
+print("the result is: ", result6)
+print("the result is: ", result7)
+print("the result is: ", result8)
+print("the result is: ", result9)
+print("the result is: ", result10)
+#7) ჩამოწერეთ ხუთი მაგალითი  !=(not equal) ოპერატორზე.
+print("davaleba 7")
+def ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(num1, num2):
+    product = num1 != num2
+    if num1!=num2:
+        return product
+result6 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 5)
+result7 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 6)
+result8 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 8)
+result9 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 10)
+result10 = ჩამოწერეთ_ხუთი_მაგალითი_მეტობატოლობა_ოპერატორზე(4, 12)
+print("the result is: ", result6)
+print("the result is: ", result7)
+print("the result is: ", result8)
+print("the result is: ", result9)
+print("the result is: ", result10)
+#8) მომხმარებელს შემოატანინეთ ორი რიცხვი და ყველა შესწავლილი ოპერატორით მოახდინეთ მათზე შედარებები.
+print("davaleba 8")
+momxmarebeli=int(input("chawere pirveli ricxvi "))
+momxmarebeli2=int(input("chawere meore ricxvi: "))
+print(momxmarebeli>momxmarebeli2)
+print(momxmarebeli<momxmarebeli2)
+print(momxmarebeli==momxmarebeli2)
+print(momxmarebeli<=momxmarebeli2)
+print(momxmarebeli!=momxmarebeli2)
+print(momxmarebeli>=momxmarebeli2)
+#9) მომხმარებელს შემოატანინეთ რიცხვი და ყველა შესწავლილი ოპერატორით შეადარეთ ის მასზე 10-ით დიდ რიცხვს.
+momxmarebeli3=int(input("sheitane ricxvi: "))
+operatorilavi=momxmarebeli3+10
+print(momxmarebeli3>operatorilavi)
+print(momxmarebeli3<operatorilavi)
+print(momxmarebeli3==operatorilavi)
+print(momxmarebeli3<=operatorilavi)
+print(momxmarebeli3!=operatorilavi)
+print(momxmarebeli3>=operatorilavi)
