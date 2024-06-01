159367452f2386bf8baff6dbea5908d5bcb0d2c2
nika tavartqiladze <165582473+Nika1232@users.noreply.github.com>
2024-06-01 09:29:38 +0400

    Add files via upload

---
+++ Day 06/Level 6 CROOK.py
@@ -0,0 +1,21 @@
+erti= int(input("chawere pirveli ricxvi: "))
+ori= int(input("chawere meore ricxvi: "))
+sami= int(input("chawere mesame ricxvi: "))
+otxi= int(input("chawere meotxe ricxvi: "))
+
+print(erti*ori*sami*otxi)
+print(erti/ori/sami/otxi)
+print(erti+ori+sami+otxi)
+print(erti-ori-sami-otxi)
+print(erti>ori)
+print(erti<ori)
+print(ori<erti)
+print(ori>erti)
+
+saxeli=input("chawere ra gqvia: ")
+gvari=input("chawere ra gvaris xar: ")
+asaki=input("ramdeni wlis xar: ")
+emaili=input("chawere sheni emaili: ")
+print("sheni saxelia: ", saxeli,"sheni gvaria: ",gvari,"sheni asakia: ",asaki,"sheni emailia: ",emaili)
+
+
